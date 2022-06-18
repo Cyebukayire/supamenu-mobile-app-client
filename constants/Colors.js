b8ac14412e231fd7d598eabbeb31fd806eb26bcd
Cyebukayire <peacecyebukayire@gmail.com>
2022-06-19 00:03:39 +0200

    chore: signin screen

---
+++ constants/Colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    primary: '#f3931c',
+    secondary: '#f8f8fb',
+    grey: '#c7c7c7',
+    green: '#24d484',
+} 
---
+++ screens/auth/Signin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function SigninScreen(){
+    return(
+        <View>
+            <Text>SignIn</Text>
+        </View>
+    );
+} 
+
+const styles = Stylecheet.create({  
+
+}); 
